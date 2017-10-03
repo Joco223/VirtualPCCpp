b022b7c8e084605314258f9b5ca3bc84a38c7927
Joco223 <jovanmihajlovic22@gmail.com>
2017-10-03 20:28:25 +0200

    GPUOld removal

--- VirtualPCCpp/NSSDL.h
+++ VirtualPCCpp/NSSDL.h
@@ -3,7 +3,7 @@
 class SDLWindow;
 
 namespace NSSDL {
-	int initSDL(SDLWindow*& window, int& width, int &height);
-	void updateSDL(SDLWindow*& window);
-	void cleanSDL(SDLWindow*& window);
+	int initSDL(SDLWindow* window, int& width, int &height);
+	void updateSDL(SDLWindow* window);
+	void cleanSDL(SDLWindow* window);
 }
