2206aeb77969cb836e130cbd249f3ab01618a486
Joco223 <jovanmihajlovic22@gmail.com>
2018-02-06 10:47:03 +0100

    Memory update

--- source/Memory.cpp
+++ source/Memory.cpp
@@ -7,13 +7,13 @@ Memory::Memory(int size) {
 
 void Memory::Clear() {
 	for (unsigned int i = 0; i < memory.size(); i++) {
-		memory[(int)i] = 0;
+		memory[i] = 0;
 	}
 }
 
 bool Memory::Empty() {
 	for (unsigned int i = 0; i < memory.size(); i++) {
-		if (memory[(int)i] != 0) {
+		if (memory[i] != 0) {
 			return false;
 		}
 	}
